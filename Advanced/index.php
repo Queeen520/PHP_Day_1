1fb817cf95492cefa53a40491e58d88def8e8d50
Isabella W <isabellawindhaber@gmail.com>
2022-11-07 13:08:25 +0100

    update advanced

---
+++ Advanced/index.php
@@ -0,0 +1,36 @@
+<?php 
+$fav_simpson = [
+    "name" => "Homer Simpson",
+    "from" => "The Simpsons",
+    "creator" => "Matthew „Matt“ Groening",
+    "release_year" => 1989
+];
+
+$fav_simpson["official_website"] = "https://TheSimpsons.com"; #adding a new value
+
+$game_characters = [];
+$movie_characters = [];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!--
+
+Define an associative array and output array elements on the screen. 
+Use for this cartoon/anime/game characters (like Mickey Mouse, Super Mario, Goku, Pokemon etc.). 
+
+Transform this code into a solution done with multidimensional arrays to track different properties 
+of the characters. (hint: in order to output nicely (user friendly) your solutions on the browser 
+you will need the HTML tags for it).  
+
+-->
+
+</body>
+</html>
