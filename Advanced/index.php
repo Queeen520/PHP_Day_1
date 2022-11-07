70f35434a6777eeef392b20e98c313fddda29d8f
Isabella W <isabellawindhaber@gmail.com>
2022-11-07 14:21:32 +0100

    ...

--- Advanced/index.php
+++ Advanced/index.php
@@ -1,15 +1,43 @@
 <?php 
-$fav_simpson = [
-    "name" => "Homer Simpson",
-    "from" => "The Simpsons",
+/*
+
+Define an associative array and output array elements on the screen. 
+Use for this cartoon/anime/game characters (like Mickey Mouse, Super Mario, Goku, Pokemon etc.). 
+
+Transform this code into a solution done with multidimensional arrays to track different properties 
+of the characters. (hint: in order to output nicely (user friendly) your solutions on the browser 
+you will need the HTML tags for it).  
+
+*/
+
+$simpsons = [
+    "cartoon_name" => "The Simpsons",
     "creator" => "Matthew „Matt“ Groening",
-    "release_year" => 1989
+    "release_year" => 1989,
+    "characters" => array(
+        "mom" => "Marge Simpson", 
+        "dad" => "Homer Simpson", 
+        "son" => "Bart Simpson", 
+        "daughter" => "Lisa Simpson")
 ];
 
-$fav_simpson["official_website"] = "https://TheSimpsons.com"; #adding a new value
+$simpsons["official_website"] = "https://TheSimpsons.com"; #adding a new value
+
+$pulp_fiction = [
+    "creator" => "Quentin Tarantino",
+    "release_year" => 1994,
+    "FSK" => 16,
+    "characters" => array(
+        "Vincent Vega" => "John Travolta", 
+        "Jules Winnfield" => "Samuel L. Jackson",
+        "Mia Wallace" => "Uma Thurman",
+        "Butch" => "Bruce Willis",
+        "Marsellus Wallace" => "Ving Rhames"
+    )
+];
 
 $game_characters = [];
-$movie_characters = [];
+
 ?>
 
 <!DOCTYPE html>
@@ -21,16 +49,18 @@ $movie_characters = [];
     <title>Document</title>
 </head>
 <body>
-<!--
 
-Define an associative array and output array elements on the screen. 
-Use for this cartoon/anime/game characters (like Mickey Mouse, Super Mario, Goku, Pokemon etc.). 
+<?php 
 
-Transform this code into a solution done with multidimensional arrays to track different properties 
-of the characters. (hint: in order to output nicely (user friendly) your solutions on the browser 
-you will need the HTML tags for it).  
+echo "<pre>";
+print_r($pulp_fiction); 
+echo "</pre>";
 
--->
+echo "<pre>";
+echo $pulp_fiction["characters"]["Vincent Vega"]; 
+echo "</pre>";
+
+?>
 
 </body>
 </html>
